befc06536267aa0ee77a95568cb234b555767fa7
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-25 11:37:57 -0600

    finished project

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,6 +1,23 @@
-angular.module('quoteBook').controller('mainCtrl', function($scope) {
+angular.module('quoteBook').controller('mainCtrl', function($scope, dataService) {
 
-    $scope.test = 'My name is Dallin'
+    $scope.quotes = dataService.getQuotes();
+
+    $scope.removeData = function(textToRemove){
+      dataService.removeData(textToRemove);
+    }
+
+    $scope.addData = function(text, author){
+      var newQuote = {
+        text: text,
+        author: author
+      }
+      if(dataService.addData(newQuote))
+      {
+        $scope.newQuoteText = '';
+        $scope.newQuoteAuthor = '';
+      }
+    }
+
 
 
 });
