c721f1e34529548760f857c70eef47a4a68ea6f2
dallin-r-parker <dallin.r.parker@gmail.com>
2016-10-24 22:45:54 -0600

    file structure & basic app running

---
+++ js/app.js
@@ -0,0 +1,5 @@
+angular.module('quoteBook', [])
+
+
+//Insert third-party dependencies into empty array brackets
+//example: ng-grid, ui.router, etc...
---
+++ js/mainCtrl.js
@@ -0,0 +1,6 @@
+angular.module('quoteBook').controller('mainCtrl', function($scope) {
+
+    $scope.test = 'My name is Dallin'
+
+
+});
